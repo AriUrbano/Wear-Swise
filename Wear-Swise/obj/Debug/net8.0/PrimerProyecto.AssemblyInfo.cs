d563a4ef6eb7071f4a8c50fd4edeed0e824ba164
AriUrbano <48113061@est.ort.edu.ar>
2025-03-30 19:23:52 -0300

    Base de datos

--- Wear-Swise/obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ Wear-Swise/obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c743f2337390ea8f4110ce9b92ee6b1f0ad3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204d1664c1b64535c84086d79c713cec7c0d8bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
